d51793391233587caa820f35e7b7dc60a11bd9ee
jiale@modefair.com <Jiale1997@>
2022-11-11 15:26:45 +0800

    add store page comparison sections.

--- src/app/modules/landing/components/product-card/product-card.component.ts
+++ src/app/modules/landing/components/product-card/product-card.component.ts
@@ -9,13 +9,14 @@ export class ProductCardComponent implements OnInit {
   @Input() imageName: string = 'ipad-pro';
   @Input() colorOptions: string = 'ipad-pro';
   @Input() isNew: boolean = true;
-  @Input() generation: string = '10th generation';
+  @Input() generation: string = '';
   @Input() productName: string = 'iPad Pro'
   @Input() description: string = 'The ultimate iPad experience with the most advanced technology.'
   @Input() price: string = 'From RM 3,899';
   @Input() buttonTitle: string = 'View pricing';
   @Input() isHalf: boolean = false;
   @Input() learnMoreVisible: boolean = true;
+  @Input() isStore: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
